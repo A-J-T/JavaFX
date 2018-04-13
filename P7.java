8422215315ff7b51aaeba2416a87c054cac69c55
Amod Tawade <Amodtawade90@yahoo.com>
2018-04-13 20:33:49 +0530

    data passing between stages

---
+++ P7.java
@@ -0,0 +1,35 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class P7 extends Application
+{
+	Stage window;
+	Button button;
+
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage)
+	{
+		window = primaryStage;
+		window.setTitle("Cool");
+		button = new Button("Click Me!");
+
+		button.setOnAction(e -> {
+			boolean result = ConfirmBox.display("Delete Files","Do you want to delete files?");
+			System.out.println(result);
+		});
+
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		Scene scene = new Scene(layout, 300, 250);
+		window.setScene(scene);
+		window.show();
+	}
+}
